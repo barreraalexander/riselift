0fd4e3e7fc592876695a58aa2cffb2f78b04dac6
barreraalexander <barreraalexander93@gmail.com>
2022-06-16 18:54:23 -0400

    added homevue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,17 @@
+<script setup>
+import StorySectionVue from '@/components/sections/StorySection.vue';
+// import AboutProgramVue from '@/components/sections/AboutProgram.vue';
+// import AboutMeVue from '@/components/sections/AboutMe.vue';
+</script>
+
 <template>
-  <h1>
-    yos
-  </h1>
+  <section id="HomeView">
+    <StorySectionVue />
+    <h1>
+      yos
+    </h1>
+
+  </section>
 </template>
 
 <!-- <script>
