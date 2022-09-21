74f499028a832b59d3cf97cb03e581b1d94d91b2
barreraalexander <barreraalexander93@gmail.com>
2022-09-21 15:41:27 -0400

    new ts frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,30 @@
-<script setup>
-import HeaderVue from './components/navbars/HeaderNav.vue';
-import FooterVue from './components/navbars/FooterNav.vue';
-</script>
-    
 <template>
-    <section id="AppSection">
-        <HeaderVue/>
-        <router-view/>
-        <FooterVue/>
-    </section>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <style lang="scss">
-@import "@/assets/scss/base/base.scss";
-@import "@/assets/scss/base/typography.scss";
-@import "@/assets/scss/base/classes.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- frontend/src/types/User.ts
+++
@@ -1,7 +0,0 @@
-interface User {
-    id: number,
-    email: string,
-    name: string,   
-}
-
-export default User
